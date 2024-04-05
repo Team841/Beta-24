09ab22c225ff1268559d36c8244c832703c52165
sara-hermosillo <139658030+sara-hermosillo@users.noreply.github.com>
2024-04-04 17:26:56 -0700

    limelight mega tag 2
    Co-Authored-By: David <82059858+Anhysteretic@users.noreply.github.com>

--- src/main/java/com/team841/betaSwerve2024/Drive/Drivetrain.java
+++ src/main/java/com/team841/betaSwerve2024/Drive/Drivetrain.java
@@ -31,19 +31,20 @@ import java.util.function.Supplier;
 public class Drivetrain extends SwerveDrivetrain implements Subsystem {
 
   /*
-  NetworkTableInstance inst = NetworkTableInstance.getDefault();
-  NetworkTable table = inst.getTable("LimelightTest");
-
-  //final StructPublisher<Pose2d> lime = table.getStructTopic("Limelight Pose", ).publish();
-
-  Topic genLime = inst.getTopic("LimelightTest");
-  Topic poseTgen = inst.getTopic("LimelightTest");
-
-  StructTopic limeT = inst.getStructTopic("LimeT", genLime.get)
-
-  final StructPublisher<Pose2d> poseP;
-  final StructPublisher<Pose2d> limeP;
-
+   * NetworkTableInstance inst = NetworkTableInstance.getDefault();
+   * NetworkTable table = inst.getTable("LimelightTest");
+   * 
+   * //final StructPublisher<Pose2d> lime = table.getStructTopic("Limelight Pose",
+   * ).publish();
+   * 
+   * Topic genLime = inst.getTopic("LimelightTest");
+   * Topic poseTgen = inst.getTopic("LimelightTest");
+   * 
+   * StructTopic limeT = inst.getStructTopic("LimeT", genLime.get)
+   * 
+   * final StructPublisher<Pose2d> poseP;
+   * final StructPublisher<Pose2d> limeP;
+   * 
    */
 
   NetworkTableInstance inst = NetworkTableInstance.getDefault();
@@ -61,8 +62,7 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
   private Notifier m_simNotifier = null;
   private double m_lastSimTime;
 
-  private final SwerveRequest.ApplyChassisSpeeds autoRequest =
-      new SwerveRequest.ApplyChassisSpeeds();
+  private final SwerveRequest.ApplyChassisSpeeds autoRequest = new SwerveRequest.ApplyChassisSpeeds();
 
   public Drivetrain(
       SwerveDrivetrainConstants driveTrainConstants,
@@ -119,9 +119,8 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
         () -> this.getState().Pose, // Supplier of current robot pose
         this::seedFieldRelative, // Consumer for seeding pose against auto
         this::getCurrentRobotChassisSpeeds,
-        (speeds) ->
-            this.setControl(
-                autoRequest.withSpeeds(speeds)), // Consumer of ChassisSpeeds to drive the robot
+        (speeds) -> this.setControl(
+            autoRequest.withSpeeds(speeds)), // Consumer of ChassisSpeeds to drive the robot
         new HolonomicPathFollowerConfig(
             new PIDConstants(10, 0, 0),
             new PIDConstants(10, 0, 0),
@@ -150,16 +149,15 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
     m_lastSimTime = Utils.getCurrentTimeSeconds();
 
     /* Run simulation at a faster rate so PID gains behave more reasonably */
-    m_simNotifier =
-        new Notifier(
-            () -> {
-              final double currentTime = Utils.getCurrentTimeSeconds();
-              double deltaTime = currentTime - m_lastSimTime;
-              m_lastSimTime = currentTime;
+    m_simNotifier = new Notifier(
+        () -> {
+          final double currentTime = Utils.getCurrentTimeSeconds();
+          double deltaTime = currentTime - m_lastSimTime;
+          m_lastSimTime = currentTime;
 
-              /* use the measured time delta, get battery voltage from WPILib */
-              updateSimState(deltaTime, RobotController.getBatteryVoltage());
-            });
+          /* use the measured time delta, get battery voltage from WPILib */
+          updateSimState(deltaTime, RobotController.getBatteryVoltage());
+        });
     m_simNotifier.startPeriodic(kSimLoopPeriod);
   }
 
@@ -173,55 +171,62 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
     return LimelightHelpers.getBotPoseEstimate_wpiBlue(Swerve.Vision.kLimelightFrontName);
   }
 
-  public Supplier<Rotation2d> getHeadingToSpeaker =
-      () -> {
-        Rotation2d aimGoal;
+  public Supplier<Rotation2d> getHeadingToSpeaker = () -> {
+    Rotation2d aimGoal;
 
-        if (ConstantsIO.isRedAlliance.get()) { // Red side
-          if (Math.abs(this.getState().Pose.getY() - Field.kRedSpeakerPose2d.getY()) < 0.15) {
-            // aimGoal = new Rotation2d(Math.toRadians(-1 *
-            // this.getState().Pose.getRotation().getDegrees()));
-            aimGoal = new Rotation2d(0);
-          } else {
-            aimGoal =
-                new Rotation2d(
-                    Math.atan(
-                        (Field.kRedSpeakerPose2d.getY() - this.getState().Pose.getY())
-                            / (Field.kRedSpeakerPose2d.getX() - this.getState().Pose.getX())));
-          }
-        } else { // blue side
-          if (Math.abs(this.getState().Pose.getY() - Field.kBlueSpeakerPose2d.getY()) < 0.15) {
-            // aimGoal = new
-            // Rotation2d(Math.toRadians(this.getState().Pose.getRotation().getDegrees() - 180));
-            aimGoal = new Rotation2d(Math.PI);
-          } else {
-            aimGoal =
-                new Rotation2d(
-                    Math.atan(
-                            (Field.kBlueSpeakerPose2d.getY() - this.getState().Pose.getY())
-                                / (Field.kBlueSpeakerPose2d.getX() - this.getState().Pose.getX()))
-                        + 180);
-          }
-        }
+    if (ConstantsIO.isRedAlliance.get()) { // Red side
+      if (Math.abs(this.getState().Pose.getY() - Field.kRedSpeakerPose2d.getY()) < 0.15) {
+        // aimGoal = new Rotation2d(Math.toRadians(-1 *
+        // this.getState().Pose.getRotation().getDegrees()));
+        aimGoal = new Rotation2d(0);
+      } else {
+        aimGoal = new Rotation2d(
+            Math.atan(
+                (Field.kRedSpeakerPose2d.getY() - this.getState().Pose.getY())
+                    / (Field.kRedSpeakerPose2d.getX() - this.getState().Pose.getX())));
+      }
+    } else { // blue side
+      if (Math.abs(this.getState().Pose.getY() - Field.kBlueSpeakerPose2d.getY()) < 0.15) {
+        // aimGoal = new
+        // Rotation2d(Math.toRadians(this.getState().Pose.getRotation().getDegrees() -
+        // 180));
+        aimGoal = new Rotation2d(Math.PI);
+      } else {
+        aimGoal = new Rotation2d(
+            Math.atan(
+                (Field.kBlueSpeakerPose2d.getY() - this.getState().Pose.getY())
+                    / (Field.kBlueSpeakerPose2d.getX() - this.getState().Pose.getX()))
+                + 180);
+      }
+    }
 
-        return aimGoal;
-      };
+    return aimGoal;
+  };
 
   @Override
   public void periodic() {
-    var PoseEstimate =
-        LimelightHelpers.getBotPoseEstimate_wpiBlue(Swerve.Vision.kLimelightFrontName);
-    if (PoseEstimate.tagCount >= 2) {
-      this.setVisionMeasurementStdDevs(VecBuilder.fill(0.7, 0.7, Math.PI));
-      this.addVisionMeasurement(PoseEstimate.pose, PoseEstimate.timestampSeconds);
-    }
-
+   boolean doRejectUpdate = false;
+      LimelightHelpers.SetRobotOrientation("limelight", this.getState().Pose.getRotation().getDegrees(), 0, 0, 0, 0, 0);
+      LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+        if(Math.abs(this.getState().speeds.omegaRadiansPerSecond) > 4 * Math.PI || mt2.tagCount <2) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+      {
+        doRejectUpdate = true;
+      }
+        if(!doRejectUpdate)
+      {
+        this.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,Math.PI*1.5));
+        this.addVisionMeasurement(
+            mt2.pose,
+            mt2.timestampSeconds);
+  }
     ctreP.set(this.getState().Pose);
-    limeP.set(PoseEstimate.pose);
+    limeP.set(mt2.pose);
 
-    SmartDashboard.putBoolean("2 tags", PoseEstimate.tagCount >= 2);
+    SmartDashboard.putBoolean("2 tags", mt2.tagCount >= 2);
 
     SmartDashboard.putNumber("Turn angle", getHeadingToSpeaker.get().getDegrees());
     SmartDashboard.putNumber("Facing", this.getState().Pose.getRotation().getDegrees());
-  }
+
+    
+}
 }
